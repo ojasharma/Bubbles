de7445e33469ee4278a8e58f90a9828c34c410fa
ojassharma <ojasvisharmacs@gmail.com>
2025-04-04 16:55:53 +0530

    CLERK CHECKPOINT

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,18 +1,22 @@
-import './globals.css'
+import "./globals.css";
 import { TransitionProvider } from "./context/transition-context";
+import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
-  title: 'Bubbles',
-  description: 'Note Taking App',
-}
-
+  title: "Bubbles",
+  description: "Note Taking App",
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body style={{ margin: 0, padding: 0 }}>
-        <TransitionProvider>{children}</TransitionProvider>
-      </body>
-    </html>
+    <ClerkProvider
+      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
+    >
+      <html lang="en">
+        <body style={{ margin: 0, padding: 0 }}>
+          <TransitionProvider>{children}</TransitionProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
