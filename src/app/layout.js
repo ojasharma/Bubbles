7b1dcad2c3dc2e349a51b23578c3bdff25729cff
ojassharma <ojasvisharmacs@gmail.com>
2025-04-05 11:42:32 +0530

    layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,22 +1,18 @@
-import "./globals.css";
+import './globals.css'
 import { TransitionProvider } from "./context/transition-context";
-import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata = {
-  title: "Bubbles",
-  description: "Note Taking App",
-};
+  title: 'Bubbles',
+  description: 'Note Taking App',
+}
+
 
 export default function RootLayout({ children }) {
   return (
-    <ClerkProvider
-      publishableKey={process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY}
-    >
-      <html lang="en">
-        <body style={{ margin: 0, padding: 0 }}>
-          <TransitionProvider>{children}</TransitionProvider>
-        </body>
-      </html>
-    </ClerkProvider>
+    <html lang="en">
+      <body style={{ margin: 0, padding: 0 }}>
+        <TransitionProvider>{children}</TransitionProvider>
+      </body>
+    </html>
   );
 }
